ccd085475ac7ba0de6fc805ab6b329bad8b8005c
Kristiyan Petrov <kristiyan.petrov@bandyer.com>
2020-11-07 00:12:38 +0100

    release v1.0.0

---
+++ kDoc/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":bandyer-android-broadcast-sdk:dokkaHtml/debug": [], ":bandyer-android-broadcast-sdk:dokkaHtml/main": [], ":bandyer-android-broadcast-sdk:dokkaHtml/release": []}'
